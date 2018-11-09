a8ed2c1d8f9b80d68468896597a9f02b3d3e0f59
Aryan Agrawal <aryanagrawal@email.arizona.edu>
2018-11-09 15:55:38 -0700

    updates

--- multithread/controller/ControllerMT.java
+++ multithread/controller/ControllerMT.java
@@ -1,28 +1,25 @@
 package controller;
 
-import javax.swing.JFrame;
-
 import multithreadedmodel.GravityMT;
 
-public class ControllerMT extends JFrame {
+public class ControllerMT {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
 
 	// Graphics view and hardware listeners
 	private GravityMT universe;
 
 	// Dimensions of the actual plane.
-	public static int UNIVERSE_WIDTH = 720*512;
-	public static int UNIVERSE_HEIGHT = 480*512;
+	public static int UNIVERSE_WIDTH = 1440;
+	public static int UNIVERSE_HEIGHT = 960;
 
 	// number of bodies on the plane
-	public static int CELESTIAL_OBJECT_COUNT = 1024;
+	public static int CELESTIAL_OBJECT_COUNT = 64;
 
-	public static int NUM_WORKERS = 8;
-	public static int TIMESTEPS = 1024*4;
+	public static int NUM_WORKERS = 4;
+	public static int TIMESTEPS = 5000;
 
 	public ControllerMT() {
 		universe = new GravityMT(CELESTIAL_OBJECT_COUNT, UNIVERSE_WIDTH, UNIVERSE_HEIGHT, NUM_WORKERS);
@@ -37,50 +34,42 @@ public class ControllerMT extends JFrame {
 		 */
 
 		int numargs = args.length;
-		int size = 32;
-		int sizes[] = new int[size];
+		System.out.println("Options:");
+		System.out.println("1. Default run, no command line arguments required\n");
+		
+		System.out.println("2. Provide the following as command line arguments:");
+		System.out.println("   Radius will be randomly generated in range 10 - 20");
+		System.out.println("\t number of workers");
+		System.out.println("\t number of bodies");
+		System.out.println("\t number of iterations (timesteps)");
+		
+		System.out.println(numargs + " Command line argumets provided");
+		
 		if (numargs == 0) {
-			System.out.println("no command line arguments specified, here are the specifications");
-			System.out.println("number of workers: 1");
-			System.out.println("number of bodies: 32");
-			System.out.println("timesteps: 500");
-			System.out.println("screen dimensions: 720x480");
-		} else if (numargs == 1) {
-			// this means read the data from a file.
-			// CELESTIAL_OBJECT_COUNT
-			// NUM_WORKERS
-			// TIMESTEPS
-		} else if (numargs == 4) {
+			System.out.println("\t number of workers: 4");
+			System.out.println("\t number of bodies: 64");
+			System.out.println("\t timesteps: 5000");
+			System.out.println("\t plane dimensions: 1440 x 960");
+		} else if (numargs == 3) {
 			// This means that the size of all objects is the same
 			NUM_WORKERS = Integer.parseInt(args[0]);
 			CELESTIAL_OBJECT_COUNT = Integer.parseInt(args[1]);
-			size = Integer.parseInt(args[2]);
-			TIMESTEPS = Integer.parseInt(args[3]);
-		} else if (numargs == 3 + Integer.parseInt(args[1])) {
-			// this means that the bodies are different sizes
-			NUM_WORKERS = Integer.parseInt(args[0]);
-			CELESTIAL_OBJECT_COUNT = Integer.parseInt(args[1]);
-			size = Integer.parseInt(args[2]);
-			for (int i = 3; i < 3 + size; i++) {
-				sizes[i - 3] = Integer.parseInt(args[i]);
-			}
-			TIMESTEPS = Integer.parseInt(args[3 + size]);
+			TIMESTEPS = Integer.parseInt(args[2]);
 		}
 
 		ControllerMT universeController = new ControllerMT();
 
-		
-			long startTime = System.nanoTime();
-			universeController.universe.updateDynamics(TIMESTEPS);
-			
-			long endTime = System.nanoTime();
-			long microseconds = (endTime - startTime) / 1000;
-			long s = microseconds / 1000000;
-			long ms = microseconds % 1000000;
-			System.out.println("Time taken by "+NUM_WORKERS+" threads: " + s + " s, " + ms + "ms");
-			System.out.println("Total Collisions:");
-			System.out.println("\t Collision by Walls: " + universeController.universe.getSideBarsCollisions());
-			System.out.println("\t Total interbody collisions: " + universeController.universe.getInterBodyCollision());
-		
+		long startTime = System.nanoTime();
+		universeController.universe.updateDynamics(TIMESTEPS);
+
+		long endTime = System.nanoTime();
+		long microseconds = (endTime - startTime) / 1000;
+		long s = microseconds / 1000000;
+		long ms = microseconds % 1000000;
+		System.out.println("Time taken by " + NUM_WORKERS + " threads: " + s + " s, " + ms + "ms");
+		System.out.println("Total Collisions:");
+		System.out.println("\t Collision by Walls: " + universeController.universe.getSideBarsCollisions());
+		System.out.println("\t Total interbody collisions: " + universeController.universe.getInterBodyCollision());
+
 	}
 }
